2bf544174f437c7f8fb57757472a0d08bb9941ed
KshitijH123 <kshitijhapase@gmail.com>
2024-08-17 21:03:09 +0530

    edit commit

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -10,6 +10,12 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Home Screen'),
         actions: [
           IconButton(
+            icon: const Icon(Icons.notification_add, size: 24),
+            onPressed: () {
+              print('Shopping Icon Pressed');
+            },
+          ),
+          IconButton(
             icon: const Icon(Icons.favorite, size: 24),
             onPressed: () {
               print('Heart Icon Pressed');
@@ -25,14 +31,35 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          // Search bar at the top
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Search',
-                border: const OutlineInputBorder(),
-                prefixIcon: const Icon(Icons.search),
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.circular(30.0), 
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3), 
+                  ),
+                ],
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  hintText: 'Search',
+                  border: OutlineInputBorder(
+                    borderRadius:
+                        BorderRadius.circular(30.0), // Circular border radius
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: const Icon(Icons.search),
+                ),
               ),
             ),
           ),
