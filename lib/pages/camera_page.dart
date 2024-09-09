32d8fbb97464ae9fedbe9519ab8b3228c695ff3d
KshitijH123 <kshitijhapase@gmail.com>
2024-09-09 22:19:21 +0530

    camera access

---
+++ lib/pages/camera_page.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({Key? key}) : super(key: key);
+
+  @override
+  _CameraPageState createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+
+  Future<void> _pickImage(ImageSource source) async {
+    final XFile? image = await _picker.pickImage(source: source);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Camera'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_image != null)
+              Image.file(
+                File(_image!.path),
+                height: 300,
+                width: 300,
+                fit: BoxFit.cover,
+              )
+            else
+              const Text('No image selected.'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _pickImage(ImageSource.camera),
+              child: const Text('Take a Photo'),
+            ),
+            ElevatedButton(
+              onPressed: () => _pickImage(ImageSource.gallery),
+              child: const Text('Pick from Gallery'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
