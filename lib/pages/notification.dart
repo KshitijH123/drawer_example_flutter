9c2c424a2d2f6bfbbdb27aee21d2918a9c663a2a
KshitijH123 <kshitijhapase@gmail.com>
2024-09-24 17:14:12 +0530

    delete button

--- lib/pages/notification.dart
+++ lib/pages/notification.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class NotificationsPage extends StatelessWidget {
-  final List<Map<String, String>> notifications = [
+class NotificationsPage extends StatefulWidget {
+  const NotificationsPage({super.key});
+
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  List<Map<String, String>> notifications = [
     {
       'title': 'Order Shipped',
       'description': 'Your order #1234 has been shipped!',
@@ -24,33 +31,47 @@ class NotificationsPage extends StatelessWidget {
     },
   ];
 
+  void _deleteNotification(int index) {
+    setState(() {
+      notifications.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Notifications'),
+        title: const Text('Notifications'),
       ),
       body: ListView.builder(
         itemCount: notifications.length,
         itemBuilder: (context, index) {
-          return Card(
-            margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: ListTile(
-              leading: Icon(Icons.notifications, color: Colors.blue),
-              title: Text(
-                notifications[index]['title']!,
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(notifications[index]['description']!),
-                  SizedBox(height: 4),
-                  Text(
-                    notifications[index]['time']!,
-                    style: TextStyle(color: Colors.grey, fontSize: 12),
-                  ),
-                ],
+          return GestureDetector(
+            onTap: () => _deleteNotification(index), 
+            child: Card(
+              margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+              child: ListTile(
+                leading: const Icon(Icons.notifications, color: Colors.blue),
+                title: Text(
+                  notifications[index]['title']!,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(notifications[index]['description']!),
+                   const SizedBox(height: 4),
+                    Text(
+                      notifications[index]['time']!,
+                      style: const TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                  ],
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete, color: Colors.blue[200]),
+                  onPressed: () => _deleteNotification(index),
+                  tooltip: 'Delete Notification',
+                ),
               ),
             ),
           );
