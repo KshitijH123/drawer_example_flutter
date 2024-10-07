3b0688aeb685c0db67ec155f7a8791180a2e826f
KshitijH123 <kshitijhapase@gmail.com>
2024-10-07 23:17:39 +0530

    changes

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -15,7 +15,7 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Settings'),
+        title: const Text('Settings',style: TextStyle(fontWeight:FontWeight.w700),),
       ),
       body: ListView(
         padding: const EdgeInsets.all(16.0),
